3c423d8d5dcb81139b50e5cf71c51b49cb3b317e
Hassanfcs <ibn.e.hasssan@gmail.com>
2020-11-20 20:36:15 +0500

    Done with all CRUD operations

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root 'view#home'
   get 'about', to:'view#about'
 
-  resources :articles, only: [:show, :index, :new, :create]
+  resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
 end
