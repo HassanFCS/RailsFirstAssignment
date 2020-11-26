8bc913472376e716df8c30381163ef41fc58a0bf
Hassanfcs <ibn.e.hasssan@gmail.com>
2020-11-26 16:16:45 +0500

    Sign up form design

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   get 'about', to:'view#about'
 
   resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
+
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 end
