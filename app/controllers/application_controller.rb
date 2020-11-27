c0faf459752ed924a95bbf9a18184ffd0e729baf
Hassanfcs <ibn.e.hasssan@gmail.com>
2020-11-27 18:06:23 +0500

    Done with the Restritions from UI

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  
+  helper_method :current_user, :logged_in?
+  #this current user is also available in application_helper
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
 end
