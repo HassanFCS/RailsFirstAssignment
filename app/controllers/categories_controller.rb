7da1be3c672c6df6412c02069378ec13959eeb2c
Hassanfcs <ibn.e.hasssan@gmail.com>
2020-11-30 21:33:14 +0500

    Categories Added

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,27 @@
+class CategoriesController < ApplicationController
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "Category is successfully created"
+      redirect_to @category
+    else
+      render 'new'
+    end
+  end
+  def new
+    @category = Category.new
+  end
+
+  def index
+    @categories = Category.paginate(page: params[:page], per_page: 5)
+  end
+
+  def show
+    @category = Category.find(params[:id])
+  end
+
+  private
+  def category_params
+    params.require(:category).permit(:name)
+  end
+end
