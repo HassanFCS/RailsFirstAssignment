bce3d8993ae9a2872b596e27cbedb067b447ba99
Hassanfcs <ibn.e.hasssan@gmail.com>
2020-12-01 18:27:56 +0500

    UI for new articles done

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,8 @@
 class Article < ApplicationRecord
   belongs_to :user
+  has_many :article_categories
+  has_many :categories, through: :article_categories
+
   #validation :OnAttribute, existance: true
   validates :title, :description, presence: true
 end
